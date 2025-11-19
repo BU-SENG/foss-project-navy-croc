b3904945e6fd52df77c87d26149e13c76bb692b8
harrynengi <tammyharry13@gmail.com>
2025-11-20 00:47:34 +0100

    Delete OpenLearn/auth.js

--- OpenLearn/auth.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    const role = document.getElementById("role").value;
-    localStorage.setItem("userRole", role);
-    window.location.href = "dashboard.html";
-});
